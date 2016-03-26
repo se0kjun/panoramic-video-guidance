54977cd0adb05982ac19d774db9917f99c22ee7c
SeokJun Hong <hong921122@gmail.com>
2016-03-27 01:18:34 +0900

    :eyes: transparent preview object

--- Assets/Script/cameractl/ShowPreview.cs
+++ Assets/Script/cameractl/ShowPreview.cs
@@ -27,14 +27,18 @@ public class ShowPreview : MonoBehaviour
     private Dictionary<string, PreviewTexture> previewObject;
 
     //settings of preview object
+    private bool previewAvailable;
     private float previewObjectAlpha;
-    private float previewObjectSize;
-    private float previewObjectDelay;
+    private bool previewDelay;
+    private float previewDelayTime;
 
     void Start()
     {
         managerObject = GameObject.Find("GameManager").GetComponent<GameManager>();
         previewPlane = GameObject.Find("PreviewPlane");
+        previewAvailable = managerObject.previewCameraAvailable;
+        previewDelay = managerObject.previewCameraDelay;
+        previewDelayTime = managerObject.previewCameraDelayTime;
         outsideObject = managerObject.outsideCameraObject;
         previewObject = new Dictionary<string, PreviewTexture>();
         mainCam = Camera.main;
@@ -42,7 +46,9 @@ public class ShowPreview : MonoBehaviour
 
     void Update()
     {
-        if (outsideObject.Count != 0)
+        previewObjectAlpha = managerObject.previewCameraAlpha;
+
+        if (outsideObject.Count != 0 && previewAvailable)
         {
             foreach (string key in outsideObject.Keys)
             {
@@ -63,17 +69,46 @@ public class ShowPreview : MonoBehaviour
 
                     GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
                     plane.transform.parent = previewPlane.transform;
-                    plane.GetComponent<Renderer>().material.mainTexture = previewRtt;
-                    plane.transform.localPosition = Vector3.zero;
-                    plane.transform.localScale = new Vector3(.1f, .1f, .1f);
-                    plane.transform.localEulerAngles = Vector3.zero;
+
+                    Material planeMat = plane.GetComponent<Renderer>().material;
+                    planeMat.mainTexture = previewRtt;
+                    Color planeColor = planeMat.color;
+                    planeColor.a = previewObjectAlpha;
+                    planeMat.color = planeColor;
+                    planeMat.SetFloat("_Mode", 3);
+                    planeMat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                    planeMat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    planeMat.SetInt("_ZWrite", 0);
+                    planeMat.DisableKeyword("_ALPHATEST_ON");
+                    planeMat.DisableKeyword("_ALPHABLEND_ON");
+                    planeMat.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                    planeMat.renderQueue = 3000;
+
+                    plane.GetComponent<Renderer>().material = planeMat;
+                    plane.transform.localScale = new Vector3(.2f, .2f, .2f);
+                    plane.transform.localEulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
                     plane.layer = LayerMask.NameToLayer("PreviewCamera");
 
                     previewObject[key] = new PreviewTexture(plane, previewCam, previewRtt);
                 }
+                else
+                {
+                    Vector3 v3Pos = Camera.main.WorldToViewportPoint(outsideObject[key].transform.position);
+                    if (v3Pos.x >= 0.0f && v3Pos.x <= 1.0f && v3Pos.y >= 0.0f && v3Pos.y <= 1.0f && v3Pos.z > 0)
+                    {
+                        previewObject[key].previewObject.transform.GetComponent<Renderer>().enabled = false;
+                    }
+                    else
+                    {
+                        previewObject[key].previewObject.transform.GetComponent<Renderer>().enabled = true;
+                    }
+
+                    previewObject[key].previewCamera.transform.LookAt(outsideObject[key].transform.position);
+                }
             }
 
             RemovePreview();
+            ArrangePreview();
         }
     }
 
@@ -96,5 +131,26 @@ public class ShowPreview : MonoBehaviour
         {
             previewObject.Remove(key);
         }
+    }
+
+    void ArrangePreview()
+    {
+        Vector3 originPoint = new Vector3(-3.3f, 0, 2f);
+        float previewPanelInterval = 0.2f;
+        int index = 0;
+
+        foreach (string key in previewObject.Keys)
+        {
+            float width = previewObject[key].previewObject.transform.GetComponent<Renderer>().bounds.size.y;
+            previewObject[key].previewObject.transform.localPosition = 
+                new Vector3(originPoint.x + ((width + previewPanelInterval) * index), originPoint.y, originPoint.z );
+            index++;
+        }
+    }
+
+    System.Collections.IEnumerator DelayPreview(GameObject previewObj)
+    {
+        yield return new WaitForSeconds(previewDelayTime);
+        previewObj.GetComponent<Renderer>().enabled = false;
     }
 }
